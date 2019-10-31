3c9512a52c035d1eb4098ddca06d756575fb33e6
Rob Hedrick <rob@hedrickholdings.com>
2019-10-31 12:49:17 -0500

    dev testing

--- action-skill-smartthings-rest.py
+++ action-skill-smartthings-rest.py
@@ -3,6 +3,8 @@
 from snipsTools import SnipsConfigParser
 from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
+import io
+import requests
 
 CONFIG_INI = "config.ini"
 
@@ -24,9 +26,28 @@ class Mylights(object):
 
         device = intent_message.slots.device.first().value
         myaction = intent_message.slots.cmd.first().value
-        print(device)
-        print(myaction)
-        hermes.publish_end_session(intent_message.session_id, device + " " + myaction)
+        token = self.config.get("secret").get("bearer-auth-token")
+        api = self.config.get("secret").get("rest-api-url")
+        auth = 'Bearer ' + token.encode("utf-8")
+        header = {'Authorization': auth, 'Content-Type': 'application/json'}
+        DeviceIDs = ['09b0803c-cfe3-4b8a-8fc0-e8161701ade4', '2537cac5-2c28-454f-bdc2-5741ae4c44c4',  '7c792f42-a018-4664-af72-d41cf93b49df', '1d9b3329-4d6f-493d-baac-1ee84cec75e8', '5d5a7635-f1a5-40ac-ad1f-d75967545824']
+
+        if device == "lights":
+            target = "all_lights"
+        elif device == "lamps":
+            target = "lamps"
+        else:
+            target == "one_light"
+        if target == "all_lights":
+            if myaction == "on" or myaction == "off":
+                for index in range(len(DeviceIDs)):
+                    uri=api.encode("utf-8") + '/device/' + DeviceIDs[index] + '/command/' + myaction.encode("utf-8")
+                    print(url)
+                    print(header)
+                    response = get(uri, headers=header)
+                hermes.publish_end_session(intent_message.session_id, "Turning " + myaction.encode("utf-8") + " " + device.encode("utf-8"))
+        else:
+            hermes.publish_end_session(intent_message.session_id, "Be boop be be boop, somethings not right")
 
     def master_intent_callback(self,hermes, intent_message):
         coming_intent = intent_message.intent.intent_name
